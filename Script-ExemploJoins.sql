e544396bbf948c1062967c0c00c2f7c5eac94aee
Regisjr246 <regisjr246@gmail.com>
2024-03-12 10:00:02 -0300

    aprendendo

--- Script-ExemploJoins.sql
+++ Script-ExemploJoins.sql
@@ -70,7 +70,65 @@ insert into cores_frutas (cores_id,frutas_id)
 select c.id as cores_id, f.id as frutas_id
 from cores c cross join frutas f;
   
-
 select frutas_id ,cores_id   from cores_frutas cf ;
 
 
+
+create table clientes(
+id int,
+nome varchar(50),
+primary key(id)
+);
+
+
+create table pedidos(
+id int,
+clientes_id int,
+data_pedido date,
+primary key(id),
+constraint fk_pedidos_clientes 
+foreign key(clientes_id)
+references clientes(id)
+);
+
+
+insert into  clientes (id,nome) values
+('1','2024-03-1'),
+('2','2024-03-2'),
+('3','2024-03-3'),
+('4','2024-03-4'),
+('5','2024-03-5'),
+('6','2024-03-6 ');
+
+
+
+insert into  pedidos  (id,clientes_id,data_pedido) values
+('101','1','2024-03-01'),
+('102','1','2024-03-05'),
+('103','2','2024-03-02'),
+('104','2','2024-03-03'),
+('105','3','2024-03-07'),
+('106','3','2024-03-06 '),
+('107','4','2024-03-08'),
+('108','2','2024-03-09'),
+('109','5','2024-03-10');
+
+
+
+insert into pedidos (id,data_pedido) values (110,'2024-03-11');
+
+
+
+select id, nome from clientes;
+select id,clientes_id, data_pedido from pedidos  ;
+
+
+
+
+/* INNER JOIN: Retorna registros quando há pelo menos uma corrrespodencia em ambas as tabelas.*/
+
+select c.id, c.nome, p.id, p.data_pedido, p.clientes_id
+from clientes c
+inner join pedidos p on c.id = p.clientes_id;
+/*Neste exemplo, estamos  selecionado todas as colunas das tabelas(cliente e pedido)
+ * onde há uma corespondencia entre o id na tabela clientes e o cliente_id na tabela pedidos.*/
